e3a0a221242c83b590da98395d1797bde2b84e7d
Dmitry Fedorov <fedorovdima@users.noreply.github.com>
2019-09-09 10:59:44 +0300

    Added link to a static file from hello-world app

--- ubuntu-fs/var/www/html/hello.py
+++ ubuntu-fs/var/www/html/hello.py
@@ -7,7 +7,8 @@
 #from flup.server.fcgi import WSGIServer
 def app(environ, start_response):
   start_response('200 OK', [('Content-Type', 'text/html')])
-  return('''Hello world!\n''')
+  return('''<p>Hello world!</p>
+          Wanna <a href="static/image.png">image</a>?\n''')
 
 if __name__ == '__main__':
   from flup.server.fcgi import WSGIServer
